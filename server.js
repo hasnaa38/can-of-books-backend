674c9230fb75808f5518177bb1661dbf70e30f3f
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-09-20 17:56:06 +0300

    added atlas link

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.get('/test', (request, response) => { response.send('test request received')}); //Checking the server
 
 const MONGO_SERVER = process.env.MONGO_SERVER;
-mongoose.connect(`${MONGO_SERVER}/CanOfBooks`, {useNewUrlParser: true, useUnifiedTopology: true});
-app.get('/test', (request, response) => { response.send('test request received')}); //Checking the server
+mongoose.connect(`${MONGO_SERVER}`, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //The seed function is needed to instantiate the collection, after requesting it, it can be commented
 const seedBooks = require('./models/Book.model');
-/* app.get('/seed-data', (req, res) => {
+app.get('/seed-data', (req, res) => {
   seedBooks();
   res.json({'Message':'Book object created successfully'});
-}); */
+});
 
 const getBooksListController = require('./controllers/Book.controller');
 app.get('/books', getBooksListController);
